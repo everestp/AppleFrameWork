24d79665b17bb879f29e077ed6df86f371c965dd
everestp <paudeleverest09@gmails.com>
2025-01-04 17:29:03 +0545

    Initial Commit

---
+++ AppleFrameWork/AppleFrameWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameWorkApp.swift
+//  AppleFrameWork
+//
+//  Created by Everest Paudel on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFrameWork/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleFrameWork
+//
+//  Created by Everest Paudel on 1/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
